ac2d8fdc1f8e42b03526e269416b66f9d46cf59c
Gregory Durelle <gregory.durelle@gmail.com>
2014-08-14 17:35:14 +0200

    enhancing a bit presentation.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -45,11 +45,11 @@ class Repository < ActiveRecord::Base
   def get_user_infos_for(github_contributor)
     user = @github.users.get user: github_contributor.login
     contributors << Contributor.find_or_initialize_by(github_login: github_contributor.login) do |contributor|
-      contributor.github_url = user.url
-      contributor.github_avatar_url = user.avatar_url
-      contributor.gravatar_id = user.gravatar_id
-      contributor.github_location = user.location
-      contributor.github_name = user.name
+      contributor.github_url = user.try(:url)
+      contributor.github_avatar_url = user.try(:avatar_url)
+      contributor.gravatar_id = user.try(:gravatar_id)
+      contributor.github_location = user.try(:location)
+      contributor.github_name = user.try(:name)
     end
   end
 end
